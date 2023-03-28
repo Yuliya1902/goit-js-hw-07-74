7415611a98453ef859801dadbbbe8f786cece528
Yuliya1902 <116121971+Yuliya1902@users.noreply.github.com>
2023-03-28 19:06:49 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const ulEl = document.querySelector(".gallery");
+
+const marupGallery = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="galerry__item">
+<a class="gallery__link" href="${original}">
+<img class="gallery__image" src=
+"${preview}" data-source="${original}" alt="${description}"></a></li>`;
+}).join('');
+
+ulEl.insertAdjacentHTML('beforeend', marupGallery)
+ulEl. addEventListener("click", onImageClick);
+
+
+function onImageClick (event) {
+ event.preventDefault();
+ const currentItem = event.target;
+
+
+ if (currentItem.nodeName !== 'IMG') {
+  return;
+}
+
+let instance = basicLightbox.create(`
+<img class="gallery__image" src="${currentItem.dataset.source}" width="800" height="600">`)
+
+instance.show();
+
+    window.addEventListener('keydown', onCloseModalClick);
+
+}
+
+function onCloseModalClick(event) {
+    const openInstance = document.querySelector(".basicLightbox")
+    if (event.code === "Escape") {
+        openInstance.remove();
+    }
+    window.removeEventListener('keydown', onCloseModalClick); 
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const ulEl = document.querySelector('.gallery');
+
+
+
+const marupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+}).join('');
+
+itemUl.insertAdjacentHTML('beforeend', marupGallery)
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+   
+   captionsData: 'alt',
+   captionPosition: 'bottom',
+   captionDelay: 250,
+   
+ });
+
 console.log(galleryItems);
